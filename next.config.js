9b7c25b569b0c90ced238896d7c299459834fe64
luke <luke@prescryptive.com>
2023-01-20 14:01:29 -0800

    configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['bafybeial7korh5zldyo7qmz4kkeeo5tt7tybhd7jiorz2nx7iwvpzeadhi.ipfs.nftstorage.link']
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
