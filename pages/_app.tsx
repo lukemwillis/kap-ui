5f56b3891672e85eb29612a3cb787bacbb4ddcd5
luke <luke@prescryptive.com>
2023-01-21 12:37:20 -0800

    Fix height with next div

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,7 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 padding={{ base: 4, md: 8 }}
                 margin="auto"
                 maxWidth="1024px"
-                minHeight="100%"
+                height="100%"
                 direction="column"
               >
                 <Header />
