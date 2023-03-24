068c72e4b316a68a5094d778498dcc4575157c45
luke <luke@prescryptive.com>
2023-03-24 14:43:38 -0700

    GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,52 +11,71 @@ import { CartProvider } from "../context/CartProvider";
 import { NameServiceProvider } from "../context/NameServiceProvider";
 import { UsdOracleProvider } from "../context/UsdOracleProvider";
 import { ProfileProvider } from "../context/ProfileProvider";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <AccountProvider>
-        <UsdOracleProvider>
-          <CartProvider>
-            <NameServiceProvider>
-              <ProfileProvider>
-                <Head>
-                  <title>KAP</title>
-                  <meta
-                    name="viewport"
-                    content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0"
-                  />
-                  <link
-                    href="favicon.ico"
-                    rel="icon"
-                    media="(prefers-color-scheme: light)"
-                  />
-                  <link
-                    href="favicon-inverse.ico"
-                    rel="icon"
-                    media="(prefers-color-scheme: dark)"
-                  />
-                </Head>
-                <Flex
-                  padding={{ base: 4, md: 8 }}
-                  gap={{ base: 4, md: 8 }}
-                  margin="auto"
-                  maxWidth="1024px"
-                  minHeight={{ base: "calc(100% + 3em)", md: "calc(100% + 4em)" }}
-                  direction="column"
-                >
-                  <Header />
-                  <main style={{ flex: 1, display: "flex" }}>
-                    <Component {...pageProps} />
-                  </main>
-                  <Footer />
-                </Flex>
-              </ProfileProvider>
-            </NameServiceProvider>
-          </CartProvider>
-        </UsdOracleProvider>
-      </AccountProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <AccountProvider>
+          <UsdOracleProvider>
+            <CartProvider>
+              <NameServiceProvider>
+                <ProfileProvider>
+                  <Head>
+                    <title>KAP</title>
+                    <meta
+                      name="viewport"
+                      content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0"
+                    />
+                    <link
+                      href="favicon.ico"
+                      rel="icon"
+                      media="(prefers-color-scheme: light)"
+                    />
+                    <link
+                      href="favicon-inverse.ico"
+                      rel="icon"
+                      media="(prefers-color-scheme: dark)"
+                    />
+                  </Head>
+                  <Flex
+                    padding={{ base: 4, md: 8 }}
+                    gap={{ base: 4, md: 8 }}
+                    margin="auto"
+                    maxWidth="1024px"
+                    minHeight={{
+                      base: "calc(100% + 3em)",
+                      md: "calc(100% + 4em)",
+                    }}
+                    direction="column"
+                  >
+                    <Header />
+                    <main style={{ flex: 1, display: "flex" }}>
+                      <Component {...pageProps} />
+                    </main>
+                    <Footer />
+                  </Flex>
+                </ProfileProvider>
+              </NameServiceProvider>
+            </CartProvider>
+          </UsdOracleProvider>
+        </AccountProvider>
+      </ChakraProvider>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+    </>
   );
 }
 
